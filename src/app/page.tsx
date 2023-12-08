1ec841ae1c7d42ed0a634a4dee48cf4a56fe4c20
om kyada <omkyada1i@gmail.com>
2023-12-08 16:38:18 +0530

    feature/ slider implement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,71 +9,71 @@ import { useEffect, useState } from "react";
 import { API_CONFIG, HttpService } from "../services/http.service";
 
 const Home = () => {
-  const [categoriesList, setCategoriesList] = useState([]);
-  const [articlesList, setArticlesList] = useState({} as any);
-  const [activeTab, setActiveTab] = useState<string>("ALL");
+    const [categoriesList, setCategoriesList] = useState([]);
+    const [articlesList, setArticlesList] = useState({} as any);
+    const [activeTab, setActiveTab] = useState<string>("ALL");
 
-  useEffect(() => {
-    getCategoriesData();
-    getArticlesInfo();
-  }, []);
+    useEffect(() => {
+        getCategoriesData();
+        getArticlesInfo();
+    }, []);
 
-  useEffect(() => {
-    getArticlesInfo();
-  }, [activeTab]);
+    useEffect(() => {
+        getArticlesInfo();
+    }, [activeTab]);
 
-  const getCategoriesData = () => {
-    HttpService.get(API_CONFIG.path.categories)
-      .then((response: any) => {
-        const categoriesData = response.data.map(
-          (item: any) => item.attributes.title
-        );
-        categoriesData.unshift("ALL");
-        setCategoriesList(categoriesData);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+    const getCategoriesData = () => {
+        HttpService.get(API_CONFIG.path.categories)
+            .then((response: any) => {
+                const categoriesData = response.data.map(
+                    (item: any) => item.attributes.title
+                );
+                categoriesData.unshift("ALL");
+                setCategoriesList(categoriesData);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
 
-  const getArticlesInfo = () => {
-    let params;
-    if (activeTab === "ALL") {
-      params = "";
-    } else {
-      params = `&filters[categories][title][$eqi]=${activeTab}`;
-    }
-    HttpService.get(`${API_CONFIG.path.articles}${params}`)
-      .then((response: any) => {
-        setArticlesList(response);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+    const getArticlesInfo = () => {
+        let params;
+        if (activeTab === "ALL") {
+            params = "";
+        } else {
+            params = `&filters[categories][title][$eqi]=${activeTab}`;
+        }
+        HttpService.get(`${API_CONFIG.path.articles}${params}`)
+            .then((response: any) => {
+                setArticlesList(response);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
 
-  const handleTabChange = (incomingTab: string) => {
-    setActiveTab(incomingTab);
-  };
+    const handleTabChange = (incomingTab: string) => {
+        setActiveTab(incomingTab);
+    };
 
-  return (
-    <>
-      <Header />
-      <TopBanner />
-      <div className='blog-tabs-content-section container d-flex flex-direction--column pb-100 pt-130'>
-        {categoriesList.length > 0 && (
-          <Tabs
-            tabs={categoriesList}
-            activeTab={activeTab}
-            handleTabChange={handleTabChange}
-          />
-        )}
-        <BlogCard articlesList={articlesList.data} />
-      </div>
-      <Subscribe />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <TopBanner />
+            <div className="blog-tabs-content-section container d-flex flex-direction--column pb-100 pt-130">
+                {categoriesList.length > 0 && (
+                    <Tabs
+                        tabs={categoriesList}
+                        activeTab={activeTab}
+                        handleTabChange={handleTabChange}
+                    />
+                )}
+                <BlogCard articlesList={articlesList.data} />
+            </div>
+            <Subscribe />
+            <Footer />
+        </>
+    );
 };
 
 export default Home;
